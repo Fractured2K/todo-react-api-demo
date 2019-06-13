c282b0c91c49173c814a248a2b7e4d08a5b33760
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-06-13 14:30:13 -0700

    Init commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,31 @@
 import React, { Component } from 'react';
+import { Route, withRouter } from 'react-router-dom';
+import TodoList from './components/TodoList';
+import axios from 'axios';
 
 class App extends Component {
+	state = {
+		todos: []
+	};
+
+	componentDidMount() {
+		axios.get('http://localhost:5000/todos');
+	}
+
 	render() {
 		return (
 			<div>
 				<h1>Hello</h1>
+				<Route
+					exact
+					path="/:id"
+					render={props => (
+						<TodoList {...props} />
+					)}
+				/>
 			</div>
 		);
 	}
 }
 
-export default App;
+export default withRouter(App);
---
+++ client/src/components/TodoList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const TodoList = props => {
+	console.log(props);
+	return (
+		<div>
+			<h1 />
+		</div>
+	);
+};
+
+export default TodoList;
